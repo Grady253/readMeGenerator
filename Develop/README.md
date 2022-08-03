e9b083ab3f5b469339e7284a9f0a9afb4e96d12a
Grady253 <grady.andre2@gmail.com>
2022-08-02 22:41:07 -0700

    I worked on the readme file mockup  and created some questions inside the prompt.

---
+++ Develop/README.md
@@ -0,0 +1,29 @@
+# Project Name
+
+## Description
+
+## Table of Content
+| [Installation](#installation) |
+[Usage](#usage) |
+[Credits](#credits) |
+[License](#license) |
+[Badges](#badges) |
+[Feature](#features) |
+[contributors](#contributors) |
+[test](#test) |
+
+## Installation
+
+## Usage 
+
+## Credits
+
+## License
+
+## Badges
+
+## Features
+
+## Contributors
+
+## Test
--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,56 @@
 // TODO: Include packages needed for this application
+// These are my packages that are need for my application.
 const fs = require("fs");
 const inquirer = require("inquirer");
 const markdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    inquirer
+        .prompt({
+            {
+                type:"input",
+                message:"What is the title of your project?",
+                name:"Title"
+            }
+
+            {
+                type:"input",
+                message:"Please provide a breif description of project.",
+                name:"Description"
+            }
+
+            {
+                type:"input",
+                message:"How is your project installed?",
+                name:"Installation"
+            }
+
+            {
+                type:"input",
+                message:"How is your application used?",
+                name:"Usage"
+            }
+
+            {
+                type:"input",
+                message:"Did any contribute to this project? If so, who?",
+                name:"Contributors"
+            }
+            
+            {
+                type:"input",
+                messsage:"What're the instructions to operate this application?",
+                name:"Instructions"
+            }
+
+            {
+                type:"list",
+                message:"What packages would you like to use?",
+                name:"license"
+            }
+        })
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
